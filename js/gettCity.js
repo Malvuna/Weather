40b1b045cf77b12bab25d6c1a105b145c505fd22
Malvuna <3405635@gmail.com>
2022-07-07 12:47:06 +0300

    fix api url

--- js/gettCity.js
+++ js/gettCity.js
@@ -12,7 +12,7 @@ async function gettCity() {
 
 async function gettCityTwo(ip) {
   await fetch(
-    "https://geo.ipify.org/api/v2/country?apiKey=at_61iM0mo5Vtc36OG1SzfCNvt1hSUEc&ipAddress=" +
+    "https://geo.ipify.org/api/v2/country?apiKey=at_k1srgs6BSmRfRRrcJY60twztQ7Vt6&ipAddress=" +
       ip,
     {
       method: "GET",
